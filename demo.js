f183135f3401e01febfcbe548c9653f7ade01237
Greg Smith <greg@bocoup.com>
2011-10-24 17:56:13 -0400

    spiced up demo a bit

--- demo.js
+++ demo.js
@@ -1,57 +1,89 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    var canvas = document.getElementById('bbdemo');;
+    var canvas = document.getElementById('bbdemo');
     
     var world = BB.createWorld(canvas);
     
-    var myBox = world.createEntity({
+    var player = world.createEntity({
         name: 'player',
+        x: .5,
+        y: 12,
+        height: .2,
+        width: .1,
         fixedRotation: true,
-        width: .5
+        friction: .5
     });
     
-    var MOVEMENT_FORCE = 50;
-    
-    myBox.onKeydown(function(e) {
-        if (e.keyCode === 32) {
-            this.applyImpulse(30);
+    player.onKeydown(function(e) {
+
+        var i;
+        var obj;
+        var player = this;
+
+        // determine what you're standing on
+        var standingOn;
+        var pos = this.position();
+        var allUnderMe = world.findAll(pos.x, pos.y + .2, pos.x + .1, pos.y + .3);
+        for (i = 0; i < allUnderMe.length; i++) {
+            obj = allUnderMe[i];
+            if (obj !== player) {
+                standingOn = obj;
+                break;
+            }
+        }
+        
+        // jump
+        if (e.keyCode === 32 && standingOn) {
+            console.log('jumping off ' + standingOn.name);
+            this.applyImpulse(1);
             return false;
         }
-        else if (e.keyCode === 37) {
-            this.setForce(MOVEMENT_FORCE, 270);
-            return false;
+
+        // when airborn movement is restricted
+        var force = 2;
+        if (!standingOn) {
+            force = force / 2;
         }
-        else if (e.keyCode === 39) {
-            this.setForce(MOVEMENT_FORCE, 90);
-            return false;
-        }
-    });
-    
-    myBox.onKeyup(function(e) {
+
+        // move left
         if (e.keyCode === 37) {
-            this.setForce(0, 0);
+            this.setForce('movement', force, 270);
             return false;
         }
-        else if (e.keyCode === 39) {
-            this.setForce(0, 0);
+
+        // move right
+        if (e.keyCode === 39) {
+            this.setForce('movement', force, 90);
             return false;
         }
     });
     
-    myBox.onStartContact(function(b) {
-        console.log(this.name + ' touched ' + b.name);
-    });
-
-    myBox.onFinishContact(function(b) {
-        console.log(this.name + ' pulled away from ' + b.name);
-    });
-
-    myBox.onImpact(function(b, p, t) {
-        if (p > 1) {
-            console.log('collision with ' + b.name + ' ' + p);
+    player.onKeyup(function(e) {
+        // clear movement force on keyup
+        if (e.keyCode === 37) {
+            this.clearForce('movement');
+            return false;
         }
-        if (t > 1 || t < -1) {
-            console.log('friction against ' + b.name + ' ' + t);
+        else if (e.keyCode === 39) {
+            this.clearForce('movement');
+            return false;
+        }
+    });
+    
+    player.onStartContact(function(other) {
+        console.log(this.name + ' touched ' + other.name);
+    });
+
+    player.onFinishContact(function(other) {
+        console.log(this.name + ' pulled away from ' + other.name);
+    });
+
+    player.onImpact(function(other, power, tangentPower) {
+        if (power > 1) {
+            console.log('strong collision with ' + other.name + ' ' + power);
+        }
+        if (tangentPower > 1 || tangentPower < -1) {
+            console.log('strong friction against ' + other.name + ' ' + tangentPower);
         }
     });
 
@@ -60,9 +92,26 @@ document.addEventListener("DOMContentLoaded", function() {
         type: 'static',
         width: 10,
         height: .1,
-        friction: 1,
         x: 10,
         y: 13.22
+    });
+
+    world.createEntity({
+        name: 'ground',
+        type: 'static',
+        width: 4,
+        height: .1,
+        x: 4,
+        y: 5
+    });
+
+    world.createEntity({
+        name: 'ground',
+        type: 'static',
+        width: 4,
+        height: .1,
+        x: 16,
+        y: 5
     });
     
     world.createEntity({
@@ -74,26 +123,32 @@ document.addEventListener("DOMContentLoaded", function() {
     world.createEntity({
         name: 'circle',
         shape: 'circle',
-        x: 10.5,
-        y: 8
+        x: 14,
+        y: 3
     });
     
-    var poly = world.createEntity({
+    world.createEntity({
         name: 'poly',
         shape: 'polygon',
-        x: 7,
+        x: 5,
         y: 8
     });
 
-    var polyMovingUp = true;
+    var platform = world.createEntity({
+        name: 'platform',
+        fixedRotation: true,
+        height: .1
+    });
+
+    var platformMovingUp = true;
     window.setInterval(function() {
-        polyMovingUp = !polyMovingUp;
-        if (polyMovingUp) {
-            poly.setVelocity(5, 0);
+        platformMovingUp = !platformMovingUp;
+        if (platformMovingUp) {
+            platform.setVelocity('moving platform', 5, 0);
         }
         else {
-            poly.setVelocity(5, 180);
+            platform.setVelocity('moving platform', 5, 180);
         }
-    }, 2000);
+    }, 1500);
     
 }, false);
