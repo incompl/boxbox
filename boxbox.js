9375fc8163fb576fcc306bd9e0645891334263a9
Jared Williams <jaredwilli@gmail.com>
2012-05-23 15:55:11 -0400

    updated requestAnimFrame polyfill to the new way to do it

--- boxbox.js
+++ boxbox.js
@@ -15,18 +15,32 @@ Created at Bocoup http://bocoup.com
  * @_page_compact_index
  */
 
-// Paul Irish requestAnimationFrame shim
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function(){
-    return window.requestAnimationFrame || 
-           window.webkitRequestAnimationFrame || 
-           window.mozRequestAnimationFrame || 
-           window.oRequestAnimationFrame || 
-           window.msRequestAnimationFrame || 
-           function (callback){
-               window.setTimeout(callback, 1000 / 60);
-           };
-}());
+// Erik Moller's requestAnimationFrame shim
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelRequestAnimationFrame = window[vendors[x]+
+          'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}())
 
 (function() {
 
